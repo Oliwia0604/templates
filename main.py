0061f873b3db7327f43f4ed9bcd62f967e1348d8
Oliwia <oliwia.wojtaszczyk@gmail.com>
2020-09-08 01:01:42 +0200

    checking

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route('/my page/me', methods=['GET'])
+@app.route('/mypage/me', methods=['GET'])
 def abase():
-       return render_template("abase.html")
+    return render_template("contact.html")
    
 
-@app.route('/my page/about', methods=['GET'])
+@app.route('/mypage/about', methods=['GET'])
 def about():
-       return render_template("about.html")
+    return render_template("about.html")
   
 
-@app.route('/my page/contact', methods=['POST'])
+@app.route('/mypage/contact', methods=['POST'])
 def display_contact():
     data = request.form
     print(data)
-       return redirect("/mypage/contact")
+    return redirect("/mypage/contact")
